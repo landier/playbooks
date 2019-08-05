0794f1e9b30516e2a230cc87588e4262008afe64
Nicolas Landier <nicolas.landier@gmail.com>
2019-08-05 23:43:34 +0200

    add sleep

--- bootstrap.sh
+++ bootstrap.sh
@@ -39,7 +39,9 @@ rm -f /tmp/ignition.json
 
 curl https://raw.githubusercontent.com/coreos/init/master/bin/coreos-install -o /tmp/coreos-install
 curl https://raw.githubusercontent.com/landier/playbooks/master/ignition.yml -o /tmp/ignition.yml
+
 /tmp/ct-${CT_VER}-${ARCH}-${OS} < /tmp/ignition.yml > /tmp/ignition.json
+sleep 2
 
 chmod u+x /tmp/coreos-install
 
