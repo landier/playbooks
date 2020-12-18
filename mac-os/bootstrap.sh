df469716012436b29ca59c62667f8a6fb397a973
Nicolas Landier <nicolas.landier@gmail.com>
2020-12-18 19:22:27 +0100

    Add iTerm2 zsh integration

--- mac-os/bootstrap.sh
+++ mac-os/bootstrap.sh
@@ -1,2 +1,7 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew bundle install Brewfile
+
+
+curl -L https://iterm2.com/shell_integration/zsh -o ~/.iterm2_shell_integration.zsh
+
+echo "source ~/.iterm2_shell_integration.zsh" >> ~/.zshrc
