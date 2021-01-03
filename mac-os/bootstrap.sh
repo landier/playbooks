1bdbb234f6adae1f18500b49f900948354ace9a9
Nicolas Landier <nicolas.landier@gmail.com>
2021-01-03 14:58:40 +0100

    Mac: install cryptowatch

--- mac-os/bootstrap.sh
+++ mac-os/bootstrap.sh
@@ -1,7 +1,4 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew bundle install Brewfile
 
-
-curl -L https://iterm2.com/shell_integration/zsh -o ~/.iterm2_shell_integration.zsh
-
-echo "source ~/.iterm2_shell_integration.zsh" >> ~/.zshrc
+mas install 1527457231
